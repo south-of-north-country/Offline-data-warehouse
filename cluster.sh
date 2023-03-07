1e66f22a1229544cb4566b7e22eba9c6da81abb9
Nan <Nanrqhcg@163.com>
2023-03-07 16:24:30 +0800

    first commit

---
+++ cluster.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+case $1 in
+"start"){
+        echo ================== 启动 集群 ==================
+
+        #启动 Zookeeper集群
+        zk.sh start
+
+        #启动 Hadoop集群
+        hdp.sh start
+
+        #启动 Kafka采集集群
+        kf.sh start
+
+        #启动 Flume采集集群
+        f1.sh start
+
+        #启动 Flume消费集群
+        f2.sh start
+
+        };;
+"stop"){
+        echo ================== 停止 集群 ==================
+
+        #停止 Flume消费集群
+        f2.sh stop
+
+        #停止 Flume采集集群
+        f1.sh stop
+
+        #停止 Kafka采集集群
+        kf.sh stop
+
+        #停止 Hadoop集群
+        hdp.sh stop
+
+        #停止 Zookeeper集群
+        zk.sh stop
+
+};;
+esac
+
